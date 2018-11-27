1f691a8906a8245e8641b6c0b7a75b0b662ce94b
Dronch <zandrey90@mail.ru>
2018-11-27 04:01:17 -0800

    Added source

---
+++ EmailExtractor/spiders/extractor.py
@@ -0,0 +1,40 @@
+import scrapy
+import re
+from bs4 import BeautifulSoup
+from urlparse import urlparse
+
+
+class EmailExtractorSpider(scrapy.Spider):
+
+    name = "extractor"
+
+    depth = 2
+    founded = False
+    bad_ext = [".jpg", ".png", ".jpeg", ".bmp"]
+
+    def __init__(self, url, **kwargs):
+        self.allowed_domains = [urlparse(url).netloc]
+        self.start_urls = [url]
+        super(EmailExtractorSpider, self).__init__(**kwargs)
+
+    def parse(self, response):
+        try:
+            level = response.meta['level']
+        except:
+            level = 0
+
+        html = response.body_as_unicode()
+        emails = re.compile(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", re.MULTILINE | re.IGNORECASE)
+        emails = re.findall(emails, html)
+
+        if len(emails) > 0:
+            for email in emails:
+                if email.endswith(tuple(self.bad_ext)):
+                    continue
+                self.founded = True
+                yield {'email': email}
+        elif level < self.depth and not self.founded:
+            soup = BeautifulSoup(html, features='html.parser')
+            for link in soup.findAll('a', attrs={'href': re.compile("^http")}):
+                url = link.get('href')
+                yield scrapy.Request(url, callback=self.parse, meta={'level': level + 1})
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # emailExtractor-bot
 Scrapy project for extracting emails from url
+
+## Install
+```bash
+pip install scrapy
+pip install -r requirements.txt
+```
+
+## Run
+```bash
+scrapy crawl extractor -a url=https://scrape.me -a depth=3 --set FEED_URI=scraped_data.csv --set FEED_FORMAT=csv
+```
+
+
+| Arg | Desc | Require | Example |
+| :---: | :---: | :---: | :---: |
+| `url` | Target website url | `True` | `url=https://scrape.me` |
+| `depth` | Website crawling depth  | `False` | `depth=3` |
+
